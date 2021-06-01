9d5b8def5455325c319a5a1da0b6ac8399e7ed1e
SkatGame <xaxaxaxa232@mail.ru>
2021-06-01 12:26:27 +0300

    Last

---
+++ include/header.h
@@ -0,0 +1,11 @@
+// Copyright 2021 SkatGame
+
+#ifndef TEMPLATE_HEADER_H
+#define TEMPLATE_HEADER_H
+#include <string>
+#include <vector>
+#include <iostream>
+using string = std::string;
+using std::cout;
+using std::endl;
+#endif // TEMPLATE_HEADER_H
